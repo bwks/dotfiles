de69c220915045f2cdd822b3bc4c31f4d5bf1aec
Brad Searle <bradleysearle@gmail.com>
2025-11-20 00:35:07 +1000

    wezterm

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,7 @@ local config = {}
 
 config.color_scheme = 'Catppuccin Frappe'
 config.font = wezterm.font("Recursive Monospace Casual")
-config.font_size = 15.0
+config.font_size = 14.0
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 -- config.tab_bar_at_bottom = true
