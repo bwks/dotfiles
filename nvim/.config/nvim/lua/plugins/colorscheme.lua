a9b84bb39131d4a0896538a56036b8b7026caa25
Brad Searle <bradleysearle@gmail.com>
2025-11-20 21:55:03 +1000

    coloschemes

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,8 +1,37 @@
 return {
   {
+    "ellisonleao/gruvbox.nvim",
+    priority = 1000, -- ensures theme loads first
+  },
+  {
+    "shaunsingh/nord.nvim",
+    priority = 1000,
+  },
+  -- Rose Pine
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    priority = 1000,
+  },
+  -- Nightfox
+  {
+    "EdenEast/nightfox.nvim",
+    priority = 1000,
+  },
+  -- One Dark
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000,
+  },
+  -- Solarized
+  {
+    "ishan9299/nvim-solarized-lua",
+    priority = 1000,
+  },
+  {
     "catppuccin/nvim",
     name = "catppuccin",
-    priority = 1000,
+    priority = 1001,
     lazy = false, -- <--- This forces it to load at startup!
     config = function()
       require("catppuccin").setup({
